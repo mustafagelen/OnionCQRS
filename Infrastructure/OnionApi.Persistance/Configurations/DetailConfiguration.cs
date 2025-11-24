e33020341fc868c5473f9c305cd30d957a573a01
Mustafa GELEN <73999126+mustafagelen@users.noreply.github.com>
2025-11-24 17:04:25 +0300

    .

---
+++ Infrastructure/OnionApi.Persistance/Configurations/DetailConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnionApi.Domain.Entities;
+
+namespace OnionApi.Persistance.Configurations
+{
+    public class DetailConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(255);
+
+            Faker faker = new Faker("tr");
+
+
+
+        }
+    }
+}
